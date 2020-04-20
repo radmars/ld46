0737ea775fd902657806996fccc9fd7c513797d9
Adhesion <adhesionmusic@gmail.com>
2020-04-20 11:15:59 -0400

    Add intro/splash/win screens

---
+++ Assets/Scripts/PressEnter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PressEnter : MonoBehaviour {
+
+    public string nextScene;
+    public bool useText = false;
+
+	void Start () {
+        if (useText)
+        {
+            StartCoroutine(Blink());
+        }
+    }
+
+	private IEnumerator Blink()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(.5f);
+			GetComponent<Text>().enabled = !GetComponent<Text>().enabled;
+		}
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return)) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
+		}
+	}
+}
